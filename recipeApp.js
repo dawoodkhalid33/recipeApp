2a2dd3e5dc4692c67ebcd16b249f9235524716e7
DawoodKhalid <dawoodkhalid33@gmail.com>
2023-04-15 00:23:32 +0500

    3rd commit

--- recipeApp.js
+++ recipeApp.js
@@ -2,31 +2,49 @@ var imgArr = [], titleArr = [];
 var userInput;
 button = document.getElementById('button');
 button.addEventListener("click", search)
-function search() {
-    const titleElements = document.getElementsByTagName('h2');
-    while (titleElements.length > 0) {
-        titleElements[0].parentNode.removeChild(titleElements[0]);
-    }
-    const imgElements = document.getElementsByTagName('img');
-    while (imgElements.length > 0) {
-        imgElements[0].parentNode.removeChild(imgElements[0]);
-    }
 
-    userInput = document.getElementById('search').value;
-    file = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=d4d08be08d2945b1a25ae0ea1ea02baa&query=' + userInput
-    fetch(file)
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < data.results.length; i++) {
-                titleArr[i] = data.results[i].title
-                const titleElement = document.createElement('h2');
-                titleElement.innerHTML = titleArr[i];
-                document.body.appendChild(titleElement);
-                imgArr[i] = data.results[i].image;
-                const imgElement = document.createElement('img');
-                imgElement.src = imgArr[i];
-                document.body.appendChild(imgElement);
-            }
-        }
-        )
+function search() {
+  // Clear existing titles and images
+  titleArr = [];
+  imgArr = [];
+  const mainElement = document.querySelector('.main');
+  const recipeElements = mainElement.querySelectorAll('.recipe');
+  for (let i = 0; i < recipeElements.length; i++) {
+    mainElement.removeChild(recipeElements[i]);
+  }
+
+  // Fetch new data based on user input
+  userInput = document.getElementById('search').value;
+  file = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=d4d08be08d2945b1a25ae0ea1ea02baa&query=' + userInput;
+  fetch(file)
+    .then(res => res.json())
+    .then(data => {
+      // Create new div for each title and image
+      for (let i = 0; i < data.results.length; i++) {
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe');
+
+        titleArr[i] = data.results[i].title
+        const titleElement = document.createElement('h2');
+        titleElement.innerHTML = titleArr[i];
+        recipeElement.appendChild(titleElement);
+
+        imgArr[i] = data.results[i].image;
+        const imgElement = document.createElement('img');
+        imgElement.src = imgArr[i];
+        recipeElement.appendChild(imgElement);
+
+        mainElement.appendChild(recipeElement);
+
+        const recipeId = data.results[i].id;
+        const recipeUrl = `https://api.spoonacular.com/recipes/${recipeId}/ingredientWidget.json?apiKey=d4d08be08d2945b1a25ae0ea1ea02baa`;
+        fetch(recipeUrl)
+          .then(res => res.json())
+          .then(recipeData => {
+            console.log(recipeData.ingredients[1].name)
+          })
+      }
+    });
 }
+
+
